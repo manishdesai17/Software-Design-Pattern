97f8967427a54e1217472dfcc81a6ae7afd35266
Desai Manish <138022961+manishdesai17@users.noreply.github.com>
2025-02-04 09:17:48 +0530

    Add files via upload

---
+++ sdp/singletondemo.java
@@ -0,0 +1,23 @@
+class singleton {
+    private static singleton obj;
+
+    private singleton() {
+    }
+
+    public static singleton getinstance() {
+        if (obj == null) {
+            synchronized (singleton.class) {
+                if (obj == null) {
+                    obj = new singleton();
+                }
+            }
+        }
+        return obj;
+    }
+}
+
+class singletondemo {
+    public static void main(String[] args) {
+        singleton s1 = singleton.getinstance();
+    }
+}
